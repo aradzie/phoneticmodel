541b6c5dda5ed0a8af34b246b58a8828a712d39c
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2023-10-10 06:44:21 +0200

    Validate the alphabet argument

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Phonetic Model Generator
 
-This is a tool which generates natural language phonetic model 
+This is a tool which generates a natural language phonetic model 
 for [keybr.com](https://www.keybr.com). It takes a text file as 
-input and produces either JSON or a compressed binary file as output.
+input and produces either a JSON or a compressed binary file as output.
 
 - Produce a compressed binary file:
 
 ```bash
 ./bin/genmodel.sh \
-    --alphabet=' abcdefghijklmnopqrstuvwxyz' \
+    --alphabet='abcdefghijklmnopqrstuvwxyz' \
     --order=4 \
     < english.txt
 ```
@@ -17,7 +17,7 @@ input and produces either JSON or a compressed binary file as output.
 
 ```bash 
 ./bin/genmodel.sh \
-    --alphabet=' abcdefghijklmnopqrstuvwxyz' \
+    --alphabet='abcdefghijklmnopqrstuvwxyz' \
     --order=2 \
     --json \
     < english.txt
--- src/main/java/com/keybr/phoneticmodel/Alphabet.java
+++ src/main/java/com/keybr/phoneticmodel/Alphabet.java
@@ -2,35 +2,17 @@ package com.keybr.phoneticmodel;
 
 import java.text.Normalizer;
 
-// ENGLISH(" abcdefghijklmnopqrstuvwxyz")
-// FRENCH(" abcçdeéèfghijklmnopqrstuvwxyz")
-// GERMAN(" aäbcdefghijklmnoöpqrstuüvwxyzß")
-// ITALIAN(" abcdefghijklmnopqrstuvwxyz")
-// PORTUGUESE(" aáâãàbcçdeéêfghiíjklmnoóôõpqrstuúvwxyz")
-// RUSSIAN(" абвгдеёжзийклмнопрстуфхцчшщъыьэюя")
-// SPANISH(" abcdefghijklmnñopqrstuvwxyz")
-
 class Alphabet {
     final String alphabet;
 
     Alphabet(String alphabet) {
         alphabet = Normalizer.normalize(alphabet, Normalizer.Form.NFC);
         for (int i = 0; i < alphabet.length(); i++) {
-            if (i == 0) {
-                if (alphabet.charAt(i) != ' ') {
-                    throw new IllegalArgumentException("Alphabet must begin with the space character");
-                }
-            }
-            else {
-                if (!Character.isLetter(alphabet.charAt(i))) {
-                    throw new IllegalArgumentException("Alphabet contains a non-letter character");
-                }
-                if (!Character.isLowerCase(alphabet.charAt(i))) {
-                    throw new IllegalArgumentException("Alphabet contains a non-lowercase letter character");
-                }
+            if (!(Character.isLetter(alphabet.charAt(i)) && Character.isLowerCase(alphabet.charAt(i)))) {
+                throw new IllegalArgumentException("The alphabet string can only contain lowercase letters");
             }
         }
-        this.alphabet = alphabet;
+        this.alphabet = " " + alphabet;
     }
 
     public String alphabet() {
